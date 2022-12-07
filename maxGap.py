62596e26b91382375aaac4f52faf35cc9b92a69e
Ohnmar Aung <40588510+paenilay@users.noreply.github.com>
2022-12-07 08:51:43 +0800

    Update maxGap.py

--- maxGap.py
+++ maxGap.py
@@ -1,3 +1,20 @@
+'''
+Given an integer array nums, return the maximum difference between two successive elements in its sorted form. If the array contains less than two elements, return 0.
+
+You must write an algorithm that runs in linear time and uses linear extra space.
+
+Eg 1 
+Input: nums = [3,6,9,1]
+Output: 3
+Explanation: The sorted form of the array is [1,3,6,9], either (3,6) or (6,9) has the maximum difference 3.
+
+Eg 2
+Input: nums = [10]
+Output: 0
+Explanation: The array contains less than 2 elements, therefore return 0.
+'''
+
+
 class Solution:
     def maximumGap(self, nums: List[int]) -> int:
         if (len(nums) < 2):
